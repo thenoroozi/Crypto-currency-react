2e5a4b4f8bd5c034b221093edc0bb82778ad879a
Hamed Noroozi <noruzi206@gmail.com>
2024-02-04 22:49:40 +0330

    show data in table

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,13 +1,42 @@
+//charts
+import chartUp from '../../assets/chart-up.svg'
+import chartDown from '../../assets/chart-down.svg'
 
 const TableCoin = ({ coins }) => {
 
    return (
       <div>
-         {coins.map(coin => (
-            <div key={coin.id}>
-               <h4>{coin.name}</h4>
-            </div>
-         ))}
+         <table>
+            <thead>
+               <tr>
+                  <th>Coin</th>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>24h</th>
+                  <th>Total Volume</th>
+                  <th></th>
+               </tr>
+            </thead>
+            <tbody>
+               {coins.map(coin =>
+                  <tr key={coin.id}>
+                     <td>
+                        <div>
+                           <img src={coin.image} width="30px" />
+                           <span>{coin.symbol.toUpperCase()}</span>
+                        </div>
+                     </td>
+                     <td>{coin.name}</td>
+                     <td>${coin.current_price.toLocaleString()}</td>
+                     <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+                     <td>${coin.total_volume.toLocaleString()}</td>
+                     <td>
+                        <img src={coin.price_change_percentage_24h > 0 ? chartUp : chartDown} />
+                     </td>
+                  </tr>
+               )}
+            </tbody>
+         </table>
       </div>
    );
 };
