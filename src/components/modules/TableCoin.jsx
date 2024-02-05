59d5a70670a39473036801549672cb25b6f09c5a
Hamed Noroozi <noruzi206@gmail.com>
2024-02-05 21:03:03 +0330

    add styles of TableCoin component

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -3,17 +3,18 @@ import chartUp from '../../assets/chart-up.svg'
 import chartDown from '../../assets/chart-down.svg'
 //Loader
 import { RotatingLines } from 'react-loader-spinner';
-
+//styles
+import styles from "./TableCoin.module.css";
 const TableCoin = ({ coins, isLoading }) => {
 
    return (
-      <div>
+      <div className={styles.container}>
          {isLoading ? <RotatingLines
             width='50'
             height='50'
             strokeWidth="3"
-            strokeColor='#3874ff'/> :
-            <table>
+            strokeColor='#3874ff' /> :
+            <table className={styles.table}>
                <thead>
                   <tr>
                      <th>Coin</th>
@@ -50,14 +51,15 @@ const TableRow = ({
    return (
       <tr>
          <td>
-            <div>
-               <img src={image} width="30px" />
+            <div className={styles.symbol}>
+               <img src={image} />
                <span>{symbol.toUpperCase()}</span>
             </div>
          </td>
          <td>{name}</td>
          <td>${current_price.toLocaleString()}</td>
-         <td>{price_change.toFixed(2)}%</td>
+         <td className={price_change > 0 ? styles.success : styles.error}
+         >{price_change.toFixed(2)}%</td>
          <td>${total_volume.toLocaleString()}</td>
          <td>
             <img src={price_change > 0 ? chartUp : chartDown} />
