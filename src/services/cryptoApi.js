56ead6e8d541e55dd003333c849722f146db78c8
Hamed Noroozi <noruzi206@gmail.com>
2024-02-07 18:07:46 +0330

    add currency type as parameter in cryptoApi

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,7 +1,7 @@
-const BASE_URL="https://api.coingecko.com/api/v3";
-const API_KEY= "CG-UGD2n8dF3AmYi4dcdDU75gJM";
+const BASE_URL = "https://api.coingecko.com/api/v3";
+const API_KEY = "CG-UGD2n8dF3AmYi4dcdDU75gJM";
 
-const getCoinList = (page)=>{
-  return `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`;
+const getCoinList = (page, currency) => {
+   return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`;
 }
 export default getCoinList;
