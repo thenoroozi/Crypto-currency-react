fa0468ea923f03a26b7312ea1e71c7e86d77fb77
Hamed Noroozi <noruzi206@gmail.com>
2024-02-07 20:15:53 +0330

    getting data from search input

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+//api
+import { searchCoin } from '../../services/cryptoApi';
 
 function Search({ currency, setCurrency }) {
+
+   const [text, setText] = useState("");
+   const [coins, setCoins] = useState([]);
+
+   useEffect(() => {
+      const controller = new AbortController();
+      if (!text) return;
+
+      const search = async () => {
+         try {
+            const res = await fetch(searchCoin(text, { signal: controller.signal }));
+            const json = await res.json();
+            if (json.coins) { 
+               setCoins(json.coins); 
+            } else {
+                alert(json.status.error_message)
+            }
+         } catch (error) {
+            if (error.name !== "AbortError") {
+               alert(error.message)
+            }
+         }
+      }
+
+      search();
+      return () => controller.abort();
+   }, [text])
+
    return (
       <div>
-         <input type="text" />
+         <input
+            type="text"
+            placeholder='Search ...'
+            value={text}
+            onChange={e => setText(e.target.value)} />
          <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
             <option value="usd">USD</option>
             <option value="eur">EUR</option>
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -3,5 +3,11 @@ const API_KEY = "CG-UGD2n8dF3AmYi4dcdDU75gJM";
 
 const getCoinList = (page, currency) => {
    return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`;
-}
-export default getCoinList;
+};
+
+const searchCoin = query =>
+   `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
+
+
+
+export { getCoinList, searchCoin };
