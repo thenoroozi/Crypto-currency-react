a9fefe10799e357e0efccaeb3423b3f031c734ed
Hamed Noroozi <noruzi206@gmail.com>
2024-02-28 18:17:52 +0330

    change theme of header

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,18 +1,24 @@
 import styles from './Layout.module.css'
+import coinIcon from '../../public/coin icon.png'
+import github from "../../public/github.png"
 
-function Layout({children}) {
+function Layout({ children }) {
    return (
       <>
-      <header className={styles.header}>
-         <h2>Crypto currency</h2>
-         <p>
-            <a href="https://github.com/thenoroozi">Thenoroozi</a>
-         </p>
-      </header>
-      {children}
-      <footer className={styles.footer}>
-         <p>Developed by <a href="https://github.com/thenoroozi">Hamed Noroozi</a></p>
-      </footer>
+         <header className={styles.header}>
+            <p>
+               <img src={github} alt="" />
+               <a href="https://github.com/thenoroozi">Thenoroozi</a>
+            </p>
+            <div className={styles.title}>
+               <h2>Crypto currency</h2>
+               <img src={coinIcon} />
+            </div>
+         </header>
+         {children}
+         <footer className={styles.footer}>
+            <p>Developed by <a href="https://github.com/thenoroozi">Hamed Noroozi</a></p>
+         </footer>
       </>
    );
 }
